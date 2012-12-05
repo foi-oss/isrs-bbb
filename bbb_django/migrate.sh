fafa76c4b705a6525cd5d976f97c1eb4e45ff7f2
Josip Lisec <josip@jlx.cc>
2012-12-05 01:49:07 +0100

    Simplified filesystem layout

--- bbb_django/migrate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo `which python` manage.py schemamigration bbb --auto
-sudo `which python` manage.py migrate bbb
--- bbb_django/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo kill -9 `cat /var/run/django.pid`
-sudo `which python` manage.py runfcgi host=127.0.0.1 port=9090 pidfile=/var/run/django.pid minspare=1 maxspare=2
