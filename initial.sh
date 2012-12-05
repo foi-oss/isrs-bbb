e0fe46bf127566da04164e39ea74a4c01fa1d3bf
Josip Lisec <josip@jlx.cc>
2012-12-05 01:57:30 +0100

    Added upstart script

--- initial.sh
+++ initial.sh
@@ -2,5 +2,3 @@
 `which python` manage.py syncdb
 `which python` manage.py schemamigration bbb --initial
 echo 'Please update the "SALT" and "BBB_API_URL" in "bbb/local_settings.py"' 
-
-#use manage.py convert_to_south myapp to convert the old app 
